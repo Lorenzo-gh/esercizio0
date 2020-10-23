de63daa8b33e966f1a8e477649ff3432712f3221
Lorenzo-gh <72559706+Lorenzo-gh@users.noreply.github.com>
2020-10-23 17:08:02 +0200

    Add files via upload

---
+++ 0.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n1=0, n2=0, n3=0, risult=0;
+
+    cout << "inserisci il primo numero:";
+    cin >> n1;
+    cout << "inserisci il secondo numero:";
+    cin >> n2;
+    cout << "inserisci il terzo numero:";
+    cin >> n3;
+
+    risult=n1*n2*n3;
+
+    cout << "il risultato ottenuto e':" << risult;
+    return 0;
+}
